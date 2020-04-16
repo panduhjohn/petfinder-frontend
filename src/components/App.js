380847c54243a992df550218dcd3189eb89daf97
John Stilwell <johnrstilwell@codeimmersives.com>
2020-04-16 15:35:19 -0400

    skeleton added

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Can you see APP?</h1>
+        )
+    }
+}
+
+export default App
