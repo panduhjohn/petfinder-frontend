5fc5defba9e2ff50d9e61bb1bfdbaaf2463a8a53
John Stilwell <johnrstilwell@codeimmersives.com>
2020-04-21 12:23:46 -0400

    data from petfinder displaying

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
 import Dogs from './Dogs'
+import Cards from './Cards'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Can you see APP?</h1>
-                <Dogs/>
+                
+                <Dogs />
+                <Cards/>
 
             </div>
         )
--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
-import data from '../data/data'
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+// import data from '../data/data';
 
 import { Client } from '@petfinder/petfinder-js';
 
-
-
 class Dog extends Component {
     constructor() {
-        super()
+        super();
 
         this.state = {
-
-        }
+            dog: {
+                name: '',
+                breed: '',
+                size: '',
+                gender: '',
+                coat: '',
+            },
+        };
     }
 
     componentDidMount() {
@@ -27,29 +33,68 @@ class Dog extends Component {
         client.animal
             .search()
             .then((response) => {
-                console.log(response.data)
+                this.setState(
+                    {
+                        animals: response.data.animals,
+                    },
+                    () => {
+                        console.log(this.state.animals);
+                    }
+                );
             })
             .catch((error) => {
-                console.log(error)
+                console.log(error);
             });
     }
 
     apiTest = () => {
-        const url = 'https://api.petfinder.com/v2/animals'
-        axios.get(url)
-    }
+        const url = 'https://api.petfinder.com/v2/animals';
+        axios.get(url);
+    };
 
     render() {
         return (
             <div>
+                {this.state.animals
+                    ? this.state.animals.map((animal, idx) => {
+                          return (
+                              <div>
+                                  <Card>
+                                      <Image
+                                          src='/images/mal.jpg'
+                                          wrapped
+                                          ui={false}
+                                      />
+                                      <Card.Content>
+                                          <Card.Header>Name: {animal.name}</Card.Header>
+                                          <Card.Meta>Species: {animal.type}</Card.Meta>
+                                          <Card.Meta>Species: {animal.breeds.primary}</Card.Meta>
+                                          <Card.Meta>
+                                              <span className='date'>
+                                                  Joined in 2015
+                                              </span>
+                                          </Card.Meta>
+                                          <Card.Description>
+                                              Matthew is a musician living in
+                                              Nashville.
+                                          </Card.Description>
+                                      </Card.Content>
+                                      <Card.Content extra>
+                                          <p>
+                                              <Icon name='user' />
+                                              22 Friends
+                                          </p>
+                                      </Card.Content>
+                                  </Card>
+                                  
+                              </div>
+                          );
+                      })
+                    : 'something'}
                 <h1>Dogs Component</h1>
-                <p>{data.name}</p>
             </div>
         );
-            
-        
     }
 }
 
 export default Dog;
-
